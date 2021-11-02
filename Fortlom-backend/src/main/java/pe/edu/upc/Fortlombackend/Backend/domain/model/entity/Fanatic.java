d80aa5ede612bcf8f340a70db4472e0126a994b3
giusencito <66499088+giusencito@users.noreply.github.com>
2021-11-01 20:36:14 -0500

    implementacion de DDD

---
+++ Fortlom-backend/src/main/java/pe/edu/upc/Fortlombackend/Backend/domain/model/entity/Fanatic.java
@@ -0,0 +1,21 @@
+package pe.edu.upc.Fortlombackend.Backend.domain.model.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//@Getter
+//@Setter
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@With
+//@Table(name = "Fanatic")
+//public class Fanatic {
+    //@Id
+  //  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//}
