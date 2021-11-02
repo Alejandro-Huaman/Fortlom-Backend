50f65f502dec8a32c1d9146f1b8dae54ac5b3b97
giusencito <66499088+giusencito@users.noreply.github.com>
2021-11-01 20:22:56 -0500

    creacion del proyecto

---
+++ Fortlom-backend/src/main/java/pe/edu/upc/Fortlombackend/FortlomBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.Fortlombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortlomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortlomBackendApplication.class, args);
+	}
+
+}
---
+++ Fortlom-backend/src/test/java/pe/edu/upc/Fortlombackend/FortlomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.Fortlombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortlomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
